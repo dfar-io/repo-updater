199dcdfbaddf2e72368498f8ff33d85ec0b0c7f9
Dave Farinelli <d@dfar.io>
2024-08-29 09:09:19 -0400

    Add tflint (#8)

--- main.py
+++ main.py
@@ -25,15 +25,16 @@ USER = "dfar-io"
 def main():
     '''Sets all repository settings to consistent settings'''
     repos = get_repos()
-    print(f'{"Repo" : <50} {"CICD job?" : <10}')
+    print(f'{"Repo" : <50} {"CICD job?" : <10} {"TFLint job?" : <10}')
     for repo in repos:
         update_repo(repo)
         main_branch = 'main'
 
         has_cicd_job = contains_cicd_workflow_runs(repo)
-        update_branch_protection(main_branch, repo, has_cicd_job)
+        has_tflint_job = contains_tflint_workflow_runs(repo)
+        update_branch_protection(main_branch, repo, has_cicd_job, has_tflint_job)
 
-        print(f'{repo : <50} {has_cicd_job : <10}')
+        print(f'{repo : <50} {has_cicd_job : <10} {has_tflint_job : <10}')
 
 def get_repos():
     '''
@@ -74,16 +75,21 @@ def update_repo(repo):
     except requests.exceptions.HTTPError:
         sys.exit(f'Error when updating \'{repo}\' ({response.status_code}): {response.text}')
 
-def update_branch_protection(branch, repo, has_cicd_job):
+def update_branch_protection(branch, repo, has_cicd_job, has_tflint_job):
     '''
     Updates branch protections
     https://docs.github.com/en/rest/reference/branches#update-branch-protection
     '''
 
+    checks = []
+    if has_cicd_job:
+        checks.append({ 'context': 'cicd' })
+    if has_tflint_job:
+        checks.append({ 'context': 'tflint' })
     required_status_checks = {
         'url': f'{API_URL}/repos/{USER}/{repo}/branches/{branch}/protection/required_status_checks',
         'strict': True,
-        'checks': [{ 'context': 'cicd' }] if has_cicd_job else []
+        'checks': checks
     }
 
     payload = {
@@ -118,6 +124,15 @@ def contains_cicd_workflow_runs(repo):
     '''
     
     response = r.get(f'{API_URL}repos/{USER}/{repo}/actions/workflows/cicd.yml/runs')
+    return response.status_code == 200
+
+def contains_tflint_workflow_runs(repo):
+    '''
+    Checks if a repo contains 'tflint.yml' workflow runs
+    https://docs.github.com/en/rest/actions/workflow-runs#list-workflow-runs-for-a-repository
+    '''
+    
+    response = r.get(f'{API_URL}repos/{USER}/{repo}/actions/workflows/tflint.yml/runs')
     return response.status_code == 200
 
 if __name__ == "__main__":
